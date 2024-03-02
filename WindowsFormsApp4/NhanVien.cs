350fb5baac911309eefe1a3fb36cd10e3195469b
NghiaKun <108846425+KunBottom@users.noreply.github.com>
2024-03-02 09:15:49 +0700

    update

---
+++ WindowsFormsApp4/NhanVien.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp4
+{
+    public class NhanVien
+    {
+        public string id { get; set; }
+        public string ten { get; set; }
+        public string chucVu { get; set; }
+        public string diaChi { get; set; }
+        public string sdt { get; set; }
+
+    }
+}
